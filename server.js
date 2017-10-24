b9492cca1860ee43ca0f7802b0368ce6cfb2d818
Yvanna Harris <yvannaharris@yvannas-air.broward.edu>
2017-10-24 19:14:34 -0400

    Added jawsdb connection

--- server.js
+++ server.js
@@ -43,3 +43,14 @@ db.sequelize.sync().then(function() {
   });
 });
 
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: ' ',
+		database: 'burgers_db'
+	})
+}
+
