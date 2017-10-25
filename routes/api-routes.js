4c90597533a54ba445f5f566e3ba14bd0f386cec
Yvanna Harris <yvannaharris@Yvannas-MacBook-Air.local>
2017-10-25 12:57:21 -0400

    deleted controller folder

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,7 +12,7 @@ var db = require("../models");
 // =============================================================
 module.exports = function(app) {
   // GET route for getting all of the todos
-  app.get("/api", function(req, res) {
+  app.get("/api/burgers", function(req, res) {
     // findAll returns all entries for a table when used with no options
     db.Burger.findAll({}).then(function(dbBurger) {
       // We have access to the todos as an argument inside of the callback function
