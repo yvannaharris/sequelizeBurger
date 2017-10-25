64ba739047bcd571d40357263076fabbc347a1eb
Yvanna Harris <yvannaharris@yvannas-air.broward.edu>
2017-10-24 20:02:57 -0400

    added jawsdb to config file

--- server.js
+++ server.js
@@ -44,14 +44,5 @@ db.sequelize.sync().then(function() {
   });
 });
 
-if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-	connection = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: ' ',
-		database: 'burgers_db'
-	})
-}
+
 
