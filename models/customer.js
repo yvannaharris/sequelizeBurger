b2dd5602fbae7057f2e10146b4cd244cbe1806e0
Yvanna Harris <yvannaharris@yvannas-air.attlocal.net>
2018-01-01 21:48:26 -0500

    Created cusotmer model

---
+++ models/customer.js
@@ -0,0 +1,16 @@
+module.exports = function(sequelize, DataTypes) {
+	var Customer = sequelize.define("Customer", {
+		name: {
+			type: DataTypes.STRING
+		}
+	});
+
+	Customer.associate = function(models) {
+
+		Customer.hasMany(models.Burger, {
+			onDelete: "cascade"
+		});
+	};
+
+	return Customer;
+}
